e3d5c30459619f704aabb988644c3dde6a3fca99
biel_ <biel_@DESKTOP-UNKCSAO>
2025-11-04 21:38:23 -0300

    conteudo aula camera 04/11

--- TelaHome.js
+++ TelaHome.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { ActivityIndicator, StyleSheet, Text, View, Button, Image, ScrollView, TextInput, Alert, FlatList, ActivityIndicatorBase } from 'react-native';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Saudacao from './Componentes';
@@ -8,6 +8,7 @@ import {database} from './firebase';
 import {ref,set,onValue,get} from 'firebase/database';
 import MapView, {Marker} from 'react-native-maps';
 import * as Location from 'expo-location';
+import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
  
 export default function TelaHome() {
   const varlogin = '';
@@ -15,11 +16,17 @@ export default function TelaHome() {
   const [endereco,setEndereco] = useState({});
   const [cep,setCEP] = useState({});
   const [pessoa,setPessoa] = useState({});
+
   const [carro,setCarro] = useState({});
   const [idCarro,setIDCarro] = useState('');
   const [carrosLista,setCarros] = useState({});
+
   const [location,setLocation] = useState(null);
   const [errors,setErrors] = useState(null);
+
+  const [permission, requestPermission] = useCameraPermissions();
+  const [foto, setFoto] = useState(null);
+  const cameraRef = useRef(null);
 
 function salvarCarro() {
   set(ref(database,`carros/${idCarro}`),{
@@ -32,6 +39,8 @@ function salvarCarro() {
     //console.log('criei o carro ->' + data);
   //}
 }
+
+ 
 
 useEffect(() => {
   (async ()=> {
@@ -48,7 +57,6 @@ useEffect(() => {
     }
   })();
 },[])
-
 
   useEffect(()=> {
       //localizarCEP();
@@ -90,9 +98,43 @@ useEffect(() => {
           .then(result => setEndereco(result))
           .catch(error => console.log('Falha ao conectar, você está sem internet'));
   }
+
+const tirarFoto = async() =>{
+  if (cameraRef.current){
+    const fotoCapturada = await cameraRef.current.takePictureAsync();
+    setFoto(fotoCapturada.uri);
+  }
+}
+
+if (!permission) {
+    // Camera permissions are still loading.
+    return <View />;
+  }
+if (!permission.granted) {
+    // Camera permissions are not granted yet.
+    return (
+      <View>
+        <Text>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
      return(
     <View>
       <Text>Aqui é a tela de Home</Text>
+      {!foto?(
+      <>
+      <CameraView facing="back" ref={cameraRef} style={styles.camera} />
+      <Button title='Tirar Foto' onPress={tirarFoto}/>
+      </>):(
+        <>
+          <Image source={{uri:foto}} style={styles.imagem}/>
+          <Button title='Tirar foto novamente' onPress={()=> setFoto(null)}/>
+        </>
+      )
+      }
+
       <TextInput placeholder='DIGITE O CEP' onChangeText={text=> setCEP(text)}></TextInput>
       <Button onPress={()=> localizarCEP()} title='Consultar CEP'></Button>
       <Text>Endereço:{endereco.logradouro}</Text>
@@ -151,5 +193,13 @@ const styles = StyleSheet.create({
     width: 400,
     height: 400,
     marginTop: 10
+  },
+  camera:{
+    width:300, 
+    height:300
+  },
+  imagem:{
+    width:300,
+    height:300
   }
 });
